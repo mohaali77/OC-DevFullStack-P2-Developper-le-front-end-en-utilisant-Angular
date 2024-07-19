23f19aed759acf5e39ba42979e8665c92dcd32ec
mohaali77 <mzr.mohand@gmail.com>
2024-07-19 21:38:09 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { LineChartComponent } from "./components/line-chart/line-chart.component";
 import { PieChartComponent } from "./components/pie-chart/pie-chart.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, DetailsComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LineChartComponent, PieChartComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LineChartComponent, PieChartComponent, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
