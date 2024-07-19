f3b915ba8a3dc9b12aa0392ad68abb883cb2688f
mohaali77 <mzr.mohand@gmail.com>
2024-07-20 01:29:58 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { LineChartComponent } from "./components/line-chart/line-chart.component";
-import { PieChartComponent } from "./components/pie-chart/pie-chart.component";
-import { HeaderComponent } from "./components/header/header.component";
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DetailsComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, LineChartComponent, PieChartComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, DetailsComponent, NotFoundComponent, HeaderComponent, PieChartComponent, LineChartComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
