cd7f7f30765f20eb456d78cd38bc4644185467cc
mohaali77 <mzr.mohand@gmail.com>
2024-07-20 01:29:54 +0200

    Update line-chart.component.ts

--- src/app/components/line-chart/line-chart.component.ts
+++ src/app/components/line-chart/line-chart.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-line-chart',
-  standalone: true,
-  imports: [],
   templateUrl: './line-chart.component.html',
   styleUrl: './line-chart.component.scss'
 })
