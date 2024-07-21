6c28798e33ee2f4f84f6f74d744cb7f074a34192
mohaali77 <mzr.mohand@gmail.com>
2024-07-22 00:15:54 +0200

    Update header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isDetailsPage: boolean = false; 
+
+
+  constructor(private router: Router) { }
+
+
+  ngOnInit(): void {
+    this.extractDetailsSegment();
+  }
+
+  extractDetailsSegment(): void {
+    const urlSegments = this.router.url.split('/');    
+    const detailsSegment = urlSegments.find(element => element === 'details');
+    detailsSegment === 'details' ? this.isDetailsPage = true : this.isDetailsPage = false
+  }
 
 }
