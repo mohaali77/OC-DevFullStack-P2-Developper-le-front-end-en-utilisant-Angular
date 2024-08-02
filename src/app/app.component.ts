66cf898da0d01beef133ff817fd62bc0af8b7322
mohaali77 <mzr.mohand@gmail.com>
2024-08-02 18:45:05 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
 
@@ -7,16 +7,23 @@ import { OlympicService } from './core/services/olympic.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   public olympicSubscription!: Subscription;
 
   constructor(private olympicService: OlympicService) { }
 
+  /**
+   * Lors de l'initialisation du composant on charge les données olympiques initiales en s'abonnant au service olympicService.
+   */
   ngOnInit(): void {
     this.olympicSubscription = this.olympicService.loadInitialData().subscribe();
   }
 
+    /**
+   * Lors de la destruction du composant, si il y a un abonnement
+   * on se désabonne pour éviter les fuites de données. 
+   */
   ngOnDestroy(): void {
     this.olympicSubscription.unsubscribe();
   }
